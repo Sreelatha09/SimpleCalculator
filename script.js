a9d25c97c7282a86960dc2cabdffd38df55cd210
Sreelatha Gudiwada <164993336+Sreelatha09@users.noreply.github.com>
2025-06-08 17:29:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+function append(value) {
+    document.getElementById('display').value += value;
+  }
+  
+  function clearDisplay() {
+    document.getElementById('display').value = '';
+  }
+  
+  function delChar() {
+    let val = document.getElementById('display').value;
+    document.getElementById('display').value = val.slice(0, -1);
+  }
+  
+  function calculate() {
+    try {
+      document.getElementById('display').value = eval(document.getElementById('display').value);
+    } catch {
+      document.getElementById('display').value = 'Error';
+    }
+  }
+  
